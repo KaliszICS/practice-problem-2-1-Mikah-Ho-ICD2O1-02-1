3d8c53eca38ed297f7019648c82ce3254843a4ca
Mikah-Ho-ICD2O1-02 <mikah.ho@student.tdsb.on.ca>
2026-03-10 15:28:50 +0000

    w

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,10 @@
+/**
+ * File: Methods
+ * Author: Mikah Ho
+ * Date Created: Mar 10, 2026
+ * Date Last Modified: Mar 10, 2026
+ */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -5,5 +12,37 @@ public class PracticeProblem {
 	}
 
 	//Create your functions here
+
+	//q1
+	public static int sum(int num1, int num2) {
+
+		int result = num1 + num2;
+		return result;
+
+	}
+
+	//q2
+	public static int difference(int num1, int num2) {
+
+		int result = num1 - num2;
+		return result;
+
+	}
+
+	//q3
+	public static double product(double num1, double num2) {
+
+		double result = num1 * num2;
+		return result;
+
+	}
+
+	//q4
+	public static String removeFirst(String word) {
+
+		String wordNew = word.substring(1);
+		return wordNew;
+
+	}
 
 }
